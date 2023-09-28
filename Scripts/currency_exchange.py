55aa22376b10ff1d98bdba3d6cc5d7ab2825d8b3
Lukas <lukaskopcak@gmail.com>
2023-09-28 15:24:32 +0200

    Update of scripts

--- Scripts/currency_exchange.py
+++ Scripts/currency_exchange.py
@@ -2,7 +2,7 @@ import requests
 
 print("This is currency calculator")
 
-option = input("Type 1 for USD to EUR | Type 2 for EUR to USD: ")
+option = input("Type 1 for USD to EUR | Type 2 for EUR to USD | Type 3 for EUR to BGP: ")
 option = float(option)
              
 ans = input("Write ammount you want to convert: ")
@@ -35,27 +35,41 @@ def get_exchange_rate(api, from_currency, to_currency):
 #Api key for Alpha Vantage exchange    
 api = "FRMHKK9YV5A8X6VL"
 
-#Currencies to convert
-from_currency = "USD"
-to_currency = "EUR"
 
-#Get the exchange rate
-exchange_rate = get_exchange_rate(api, from_currency, to_currency)
+#Currencies conversion
+if option == 1:
+    #Currencies to convert
+    from_currency = "USD"
+    to_currency = "EUR"
+    #Get the exchange rate
+    exchange_rate = get_exchange_rate(api, from_currency, to_currency)
+    conv = ans * exchange_rate
+    print(f"Conversion from USD to EUR: {conv}")
+
+elif option ==2:
+    #Currencies to convert
+    from_currency = "USD"
+    to_currency = "EUR"
+    #Get the exchange rate
+    exchange_rate = get_exchange_rate(api, from_currency, to_currency)
+    conv = ans / exchange_rate 
+    print(f"Conversion from EUR to USD: {conv}")
+
+elif option ==3:
+    #Currencies to convert
+    from_currency = "EUR"
+    to_currency = "GBP"
+    #Get the exchange rate
+    exchange_rate = get_exchange_rate(api, from_currency, to_currency)
+    conv = ans * exchange_rate 
+    print(f"Conversion from EUR to GBP: {conv}")
+else:
+      print("You can choose only from 1-2")
 
 if exchange_rate is not None:
     print(f"Actuall rates: 1 {from_currency} = {exchange_rate} {to_currency}")
 else:
     print("Failed to retrive the exchange rate.")
-
-#Currencies conversion
-if option == 1:
-      conv = ans * exchange_rate
-      print(f"Conversion from USD to EUR: {conv}")
-elif option ==2:
-      conv = ans / exchange_rate 
-      print(f"Conversion from EUR to USD: {conv}")
-else:
-      print("You can choose only from 1-2")
 
 
 
